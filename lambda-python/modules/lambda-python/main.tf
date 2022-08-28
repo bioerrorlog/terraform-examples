40f29c27f4edb467575599f1d76b4850674c64ee
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-28 16:08:16 +0900

    fix error: resolve resource name

--- lambda-python/modules/lambda-python/main.tf
+++ lambda-python/modules/lambda-python/main.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "this" {
   filename         = data.archive_file.zip.output_path
   source_code_hash = data.archive_file.zip.output_base64sha256
 
-  role    = aws_iam_role.iam_for_lambda.arn
+  role    = aws_iam_role.this.arn
   handler = "hello_lambda.lambda_handler"
   runtime = "python3.9"
 
