45516ea345b5ebff0295f543182790f24247b25c
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-08-29 21:57:22 +0900

    add CloudWatch Logs for the Lambda

--- lambda-python/modules/lambda-python/main.tf
+++ lambda-python/modules/lambda-python/main.tf
@@ -28,6 +28,17 @@ data "archive_file" "zip" {
 
 
 ####################################################
+# CloudWatch Logs
+####################################################
+
+resource "aws_cloudwatch_log_group" "this" {
+  name = "/aws/lambda/${aws_lambda_function.this.function_name}"
+
+  retention_in_days = var.log_retention_in_days
+}
+
+
+####################################################
 # IAM
 ####################################################
 
--- lambda-python/modules/lambda-python/variables.tf
+++ lambda-python/modules/lambda-python/variables.tf
@@ -1,9 +1,16 @@
 variable "name_prefix" {
   description = "The prefix of resource names."
+  type        = string
   default     = "demo"
 
   validation {
     condition     = length(var.name_prefix) > 0
     error_message = "You must set name_prefix with the length > 0."
   }
+}
+
+variable "log_retention_in_days" {
+  description = "the number of days you want to retain the log."
+  type        = number
+  default     = 90
 }
