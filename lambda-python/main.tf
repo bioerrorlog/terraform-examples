0f58f52509534fd8ae3957bd7571074284c30ec0
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-09-02 09:05:47 +0900

    Revert "CI test: push invalid fmt code"
    This reverts commit dfdafe825397b23d34c37d4ca74df44a2647d7b6.

--- lambda-python/main.tf
+++ lambda-python/main.tf
@@ -1,5 +1,5 @@
 module "lambda_python" {
-          source = "./modules/lambda-python"
+  source = "./modules/lambda-python"
 
   name_prefix     = "demo"
   lambda_schedule = "cron(* * * * ? *)" # every min
