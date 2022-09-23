201f540e75db85d11838f41e0eb7d13d5953e9bb
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-09-23 21:05:48 +0900

    terraform fmt

--- lambda-rust/modules/lambda-rust/locals.tf
+++ lambda-rust/modules/lambda-rust/locals.tf
@@ -1,6 +1,6 @@
 locals {
   lambda_local_path     = "${path.module}/lambda"
-  lambda_bin_name = "bootstrap"
+  lambda_bin_name       = "bootstrap"
   lambda_bin_local_path = "${local.lambda_local_path}/target/lambda/lambda/${local.lambda_bin_name}"
   lambda_zip_local_path = "${local.lambda_bin_local_path}.zip"
 }
