5a29b6beb1f92c72cece9337f1d1315277f012e5
bioerrorlog <bioerrorlog.contact@gmail.com>
2022-12-18 14:41:47 +0900

    add some example

--- syntax-sandbox/map_key_separator_error.tf
+++ syntax-sandbox/map_key_separator_error.tf
@@ -1,10 +1,11 @@
 # map key starting with numbers causes error
 locals {
   map_example = {
-    key_valid = "abcd"
-    123       = "abcd"
+    key_valid = "abcd" # OK
+    123       = "abcd" # OK
     # 123key = "abcd" # Error: Missing key/value separator
     "123key" = "abcd" # OK
+    key123   = "abcd" # OK
 
     # spaces cause same error
     # 123 key = "abcd" # Error: Missing key/value separator
