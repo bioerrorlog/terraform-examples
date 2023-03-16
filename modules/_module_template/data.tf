d18a13053f0af204484c6bf676e47d6813d02f24
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-03-16 21:35:15 +0900

    update: iam policy, not iam role

--- modules/_module_template/data.tf
+++ modules/_module_template/data.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-data "aws_iam_role" "refs" {
+data "aws_iam_policy" "refs" {
   for_each = toset([
   ])
   name = each.value
