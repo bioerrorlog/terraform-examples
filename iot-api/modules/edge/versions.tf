a477af13288cf22337270e3e4e17267ba1dba728
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-03-18 17:58:37 +0900

    init: edge module

---
+++ iot-api/modules/edge/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.2.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.28"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+
+  default_tags {
+    tags = {
+      Project = "terraform-examples"
+      Owner   = "bioerrorlog"
+    }
+  }
+}
