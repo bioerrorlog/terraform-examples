176b85f4230789627c36f7bbc94a5438053f5187
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-06-16 17:47:53 +0900

    implement subnets

--- vpc/modules/network/aws_subnets.tf
+++ vpc/modules/network/aws_subnets.tf
@@ -1,8 +1,39 @@
-resource "aws_subnet" "this" {
-  vpc_id     = aws_vpc.this.id
-  cidr_block = var.subnet_cidr
+resource "aws_subnet" "public_subnet_01" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnet_01.cidr
+  availability_zone = var.public_subnet_01.az
 
   tags = {
-    Name = "this-subnet"
+    Name = "${var.sysid}-${var.env}-public-subnet01"
+  }
+}
+
+resource "aws_subnet" "public_subnet_02" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.public_subnet_02.cidr
+  availability_zone = var.public_subnet_02.az
+
+  tags = {
+    Name = "${var.sysid}-${var.env}-public-subnet02"
+  }
+}
+
+resource "aws_subnet" "private_subnet_01" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_01.cidr
+  availability_zone = var.private_subnet_01.az
+
+  tags = {
+    Name = "${var.sysid}-${var.env}-private-subnet01"
+  }
+}
+
+resource "aws_subnet" "private_subnet_02" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_02.cidr
+  availability_zone = var.private_subnet_02.az
+
+  tags = {
+    Name = "${var.sysid}-${var.env}-private-subnet02"
   }
 }
