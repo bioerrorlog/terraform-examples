75ca7d8cfdc8dc4d4d725fc297b6f93fb5cf3d82
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-06-16 19:34:26 +0900

    add acl

--- vpc/modules/network/aws_vpc.tf
+++ vpc/modules/network/aws_vpc.tf
@@ -6,6 +6,32 @@ resource "aws_vpc" "this" {
   }
 }
 
+resource "aws_network_acl" "this" {
+  vpc_id = aws_vpc.this.id
+
+  egress {
+    protocol   = "-1" # all
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "-1" # all
+    rule_no    = 100
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "${var.sysid}-${var.env}-acl"
+  }
+}
+
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 
