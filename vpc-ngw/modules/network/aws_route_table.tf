5e0f0b023e04117ebd6624aa35bea4ad303473f0
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-07-23 13:28:15 +0900

    fix: terraform plan parmanent diff

--- vpc-ngw/modules/network/aws_route_table.tf
+++ vpc-ngw/modules/network/aws_route_table.tf
@@ -22,8 +22,8 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.this.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.this.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
   }
 
   tags = {
