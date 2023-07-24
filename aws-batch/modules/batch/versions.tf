bbe637910b713995a8d935ba50483a4f64947800
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-07-25 08:31:29 +0900

    init batch module

---
+++ aws-batch/modules/batch/versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">= 1.2.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.28"
+    }
+  }
+
+  # backend "s3" {
+  #   bucket         = "mybucket"
+  #   key            = "demo/terraform.state"
+  #   region         = "ap-northeast-1"
+  #   dynamodb_table = "demo-terraform-lock"
+  #   encrypt        = true
+  # }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+
+  default_tags {
+    tags = {
+      Project = "terraform-examples"
+      Owner   = "bioerrorlog"
+    }
+  }
+}
